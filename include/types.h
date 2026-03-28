5e75ce08362547ef3885f8aa55b9e5675a591d53
= <=>
2026-03-28 11:15:55 -0500

    Defining basic structures block for the project

---
+++ include/types.h
@@ -0,0 +1,42 @@
+enum class ChargerState
+{
+    Idle,
+    VehiculeDetected,
+    PreSafeChecks, 
+    Chargin, 
+    Stoppingg, 
+    Fault
+};
+
+struct SensorData
+{
+    bool plug_inserted;
+    bool ev_ready;
+    bool user_start_request;
+    bool user_stop_request;
+
+    float voltage;
+    float current;
+    float temperature;
+
+    bool ground_fault;
+    bool relay_feedback_closed;
+}
+
+struct ActuatorCommands
+{
+    bool close_contactor;
+    bool enable_charging;
+    float current_limit;
+}
+
+struct FaultStatus {
+    bool overtemperature;
+    bool overcurrent;
+    bool ground_fault;
+    bool relay_mismatch;
+
+    bool any() const {
+        return overtemperature || overcurrent || ground_fault || relay_mismatch;
+    }
+};
